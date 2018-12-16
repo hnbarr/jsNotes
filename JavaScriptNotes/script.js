94f4a79f7ed2f28d4db0fa3555c315a4c6101134
Heather Barr <hnbarr@yahoo.com>
2018-12-16 12:17:59 -0600

    .findIndex .filter .map .forEach

--- JavaScriptNotes/script.js
+++ JavaScriptNotes/script.js
@@ -1,15 +1,46 @@
 'use strict';
 
-const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+////////// .forEach();
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+// Iterate over fruits below
+fruits.forEach(item => console.log(`I want to eat a ${item}.`))
 
+
+/////////// .map(); 
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 // Create the secretMessage array below
 const secretMessage = animals.map(animal => animal[0]);
-
 console.log(secretMessage.join(''));
 
 const bigNumbers = [100, 200, 300, 400, 500];
-
 // Create the smallNumbers array below
 const smallNumbers = bigNumbers.map(num => num/100);
-
 console.log(smallNumbers)
+
+
+////////// .filter();
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(num => {
+  return num < 250;
+})
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter(word => {
+  return word.length > 7;
+})
+
+
+////////// .findIndex();
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant';
+});
+
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's' ? true : false;
+});
+
+
+////////// .reduce();
